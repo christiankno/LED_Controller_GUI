0b4977521422e8a80f72151f34e4868991bcaca9
christiankno <christian.knoblich98@gmail.com>
2021-05-04 18:50:29 +0200

    clean up

--- LED_MQTT_Server_Adafruit_IO.py
+++ LED_MQTT_Server_Adafruit_IO.py
@@ -1,27 +1,3 @@
-
-"""
-`rgb_led.py`
-=======================================================================
-Control a RGB LED using
-Adafruit IO and Python
-
-Tutorial Link: https://learn.adafruit.com/adafruit-io-basics-color
-
-Adafruit invests time and resources providing this open source code.
-Please support Adafruit and open source hardware by purchasing
-products from Adafruit!
-
-Author(s): Brent Rubell for Adafruit Industries
-Copyright (c) 2018 Adafruit Industries
-Licensed under the MIT license.
-All text above must be included in any redistribution.
-
-Dependencies:
-    - Adafruit_Blinka
-        (https://github.com/adafruit/Adafruit_Blinka)
-    - Adafruit_CircuitPython_PCA9685
-        (https://github.com/adafruit/Adafruit_CircuitPython_PCA9685)
-"""
 # import system libraries
 import time
 from functions import send, sendW, sendMore
@@ -32,26 +8,11 @@ from statefile import ADAFRUIT_IO_KEY, ADAFRUIT_IO_USERNAME
 # Create an instance of the REST client.
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
-try: # if we have a 'color' feed
+try: 
     toggle = aio.feeds('led-toggle')
-except RequestError: # create an `color` feed
+except RequestError:
     feed = Feed(name='led-toggle')
     toggle = aio.create_feed(feed)
-
-## Create the I2C bus interface.
-#i2c_bus = I2C(SCL, SDA)
-
-## Create a simple PCA9685 class instance.
-#pca = PCA9685(i2c_bus)
-#pca.frequency = 60
-#prev_color = '#000000'
-
-#def map_range(x, in_min, in_max, out_min, out_max):
-#    """re-maps a number from one range to another."""
-#    mapped = (x-in_min) * (out_max - out_min) / (in_max-in_min) + out_min
-#    if out_min <= out_max:
-#        return max(min(mapped, out_max), out_min)
-#    return min(max(mapped, out_max), out_min)
 
 prev_toggle_val=None
 while True:
