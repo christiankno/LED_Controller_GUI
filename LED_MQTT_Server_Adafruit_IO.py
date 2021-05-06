5147807cadb08f9a7d2e093677282940680afbec
christiankno <christian.knoblich98@gmail.com>
2021-05-06 04:15:46 +0200

    adding exception handling to MQTT.send

--- LED_MQTT_Server_Adafruit_IO.py
+++ LED_MQTT_Server_Adafruit_IO.py
@@ -15,18 +15,44 @@ aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
 try: 
     toggle = aio.feeds('led-toggle')
+    state = aio.feeds('rgb-lights.state')
+    aio.feeds()
 except RequestError:
     feed = Feed(name='led-toggle')
     toggle = aio.create_feed(feed)
+    feed = Feed(name='rgb-lights.state')
+    state = aio.create_feed(feed)
 
-prev_toggle_val=None
+toggle_data=None
+prev_toggle_data=None
+state_data=None
+prev_state_data=aio.receive(state.key)
 while True:
-    # grab the `color` feed
-    toggle_val = aio.receive(toggle.key)
-    if toggle_val != prev_toggle_val:
-        print(toggle_val.value)
-        if toggle_val.value=='ON': sendMore(enable=1)
+    try:
+        toggle_data = aio.receive(toggle.key)
+        state_data = aio.receive(state.key)
+    except Excception as e:
+        print(e)
+
+    if toggle_data != prev_toggle_data:
+        print(toggle_data.value)
+        if toggle_data.value=='ON': sendMore(enable=1)
         else: sendMore(enable=0)
+
+    if state_data != prev_state_data and state_data.value != '0':
+        print(state_data.value)
+        wrgb=[int(i) for i in state_data.value.split(',')]
+        send(wrgb[1:])
+        sendW(wrgb[0])
+        try:
+            aio.send(state.key, '0')
+        except Exception as e:
+            print(e)
+
+    prev_state_data = state_data
+    prev_toggle_data = toggle_data
+    time.sleep(0.5)
+
 
         ## print rgb values and hex value
         #print('Received Color: ')
@@ -45,6 +71,3 @@ while True:
         #pca.channels[RED_PIN].duty_cycle = 65535 - int(red)
         #pca.channels[GREEN_PIN].duty_cycle = 65535 - int(green)
         #pca.channels[BLUE_PIN].duty_cycle = 65535 - int(blue)
-    prev_toggle_val = toggle_val
-    # let's wait a bit so we don't flood adafruit io's servers...
-    time.sleep(0.5)
