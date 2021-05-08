857756e1ca05abe3aa97a78694fc9048eb52a4ec
christiankno <christian.knoblich98@gmail.com>
2021-05-08 02:41:34 +0200

    adding color control over adafruit io

--- LED_MQTT_Server_Adafruit_IO.py
+++ LED_MQTT_Server_Adafruit_IO.py
@@ -16,8 +16,10 @@ aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 try: 
     toggle = aio.feeds('led-toggle')
     state = aio.feeds('rgb-lights.state')
+    colorname = aio.feeds('rgb-lights.colorname')
     aio.feeds()
-except RequestError:
+except RequestError as e:
+    print(e)
     feed = Feed(name='led-toggle')
     toggle = aio.create_feed(feed)
     feed = Feed(name='rgb-lights.state')
@@ -27,32 +29,65 @@ toggle_data=None
 prev_toggle_data=None
 state_data=None
 prev_state_data=aio.receive(state.key)
+
+color_dict={
+    'fire': '0,4095,680,0',
+    'blue': '4095,0,0,4095',
+    'white': '4095,0,0,0',
+    'max':'4095,4095,4095,4095',
+    'red':'1000,4095,0,0',
+    'green':'2000,0,4095,0',
+    'purple':'0,4095,0,620',
+    'off':'off',
+    'on':'on',
+    }
+
 while True:
     try:
         toggle_data = aio.receive(toggle.key)
         state_data = aio.receive(state.key)
-    except Excception as e:
+        colorname_data = aio.receive(colorname.key)
+
+        if colorname_data.value != '0':
+            color=colorname_data.value.lower()
+            if color in color_dict.keys():
+                color_values=color_dict[color]
+            else: print('color not recognized')
+            
+            if color_values=='on': sendMore(enable=1)
+            elif color_values=='off': sendMore(enable=0)
+            else:
+                wrgb=[int(i) for i in color_values.split(',')]
+                send(wrgb[1:])
+                sendW(wrgb[0])
+
+            try:
+                aio.send(colorname.key, '0')
+            except Exception as e:
+                print('error sending colorname value')
+                print(e)
+
+        if toggle_data != prev_toggle_data:
+            print(toggle_data.value)
+            if toggle_data.value=='ON': sendMore(enable=1)
+            else: sendMore(enable=0)
+
+        if state_data != prev_state_data and state_data.value != '0':
+            print(state_data.value)
+            wrgb=[int(i) for i in state_data.value.split(',')]
+            send(wrgb[1:])
+            sendW(wrgb[0])
+            try:
+                aio.send(state.key, '0')
+            except Exception as e:
+                print(e)
+
+        prev_state_data = state_data
+        prev_toggle_data = toggle_data
+        time.sleep(0.5)
+
+    except Exception as e:
         print(e)
-
-    if toggle_data != prev_toggle_data:
-        print(toggle_data.value)
-        if toggle_data.value=='ON': sendMore(enable=1)
-        else: sendMore(enable=0)
-
-    if state_data != prev_state_data and state_data.value != '0':
-        print(state_data.value)
-        wrgb=[int(i) for i in state_data.value.split(',')]
-        send(wrgb[1:])
-        sendW(wrgb[0])
-        try:
-            aio.send(state.key, '0')
-        except Exception as e:
-            print(e)
-
-    prev_state_data = state_data
-    prev_toggle_data = toggle_data
-    time.sleep(0.5)
-
 
         ## print rgb values and hex value
         #print('Received Color: ')
