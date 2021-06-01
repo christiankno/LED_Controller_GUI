9a1d03e1a699f166338abd9d17b719fe7d2cdcc4
christiankno <christian.knoblich98@gmail.com>
2021-06-01 22:55:42 +0200

    added some lines

--- RFPi_controller.py
+++ RFPi_controller.py
@@ -10,33 +10,31 @@ from rpi_rf import RFDevice
 
 rfdevice = None
 
-#def exithandler(signal, frame):
-#    rfdevice.cleanup()
-#    sys.exit(0)
+def exithandler(signal, frame):
+    rfdevice.cleanup()
+    sys.exit(0)
 
 
-#logging.basicConfig(level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S',
-#                    format='%(asctime)-15s - [%(levelname)s] %(module)s: %(message)s', )
+logging.basicConfig(level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S',
+                    format='%(asctime)-15s - [%(levelname)s] %(module)s: %(message)s', )
 
 parser = argparse.ArgumentParser(description='Receives a decimal code via a 433/315MHz GPIO device')
 parser.add_argument('-g', dest='gpio', type=int, default=27,
                     help="GPIO pin (Default: 27)")
 args = parser.parse_args()
 
-#signal.signal(signal.SIGINT, exithandler)
+signal.signal(signal.SIGINT, exithandler)
 rfdevice = RFDevice(args.gpio)
 rfdevice.enable_rx()
 timestamp = None
-#logging.info("Listening for codes on GPIO " + str(args.gpio))
+logging.info("Listening for codes on GPIO " + str(args.gpio))
 print('Listening')
 while True:
     if rfdevice.rx_code_timestamp != timestamp:
         timestamp = rfdevice.rx_code_timestamp
-#        logging.info(str(rfdevice.rx_code) +
-#                     " [pulselength " + str(rfdevice.rx_pulselength) +
-#                     ", protocol " + str(rfdevice.rx_proto) + "]")
-        print(str(rfdevice.rx_code) +
+        logging.info(str(rfdevice.rx_code) +
                      " [pulselength " + str(rfdevice.rx_pulselength) +
                      ", protocol " + str(rfdevice.rx_proto) + "]")
+        if str(rfdevice.rx_code)==66560: print("this is mine: 66560")
     time.sleep(0.05)
 rfdevice.cleanup()
