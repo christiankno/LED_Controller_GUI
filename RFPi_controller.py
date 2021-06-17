bdc4059137ffa184139a2895343748f16bb0146b
raspi <christian.knoblich98@gmail.com>
2021-06-17 04:50:06 +0200

    adding functions to 433hz codes

--- RFPi_controller.py
+++ RFPi_controller.py
@@ -10,33 +10,45 @@ from rpi_rf import RFDevice
 
 rfdevice = None
 
-#def exithandler(signal, frame):
-#    rfdevice.cleanup()
-#    sys.exit(0)
 
+light_args=[{'wrgb':[4095,2000,500,0]},
+    {'toggle':1},
+    {'wrgb':[0,4095,800,0]},
+    {'wrgb':[0,156,30,0]},
+    ]
 
-#logging.basicConfig(level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S',
-#                    format='%(asctime)-15s - [%(levelname)s] %(module)s: %(message)s', )
+def exithandler(signal, frame):
+    rfdevice.cleanup()
+    sys.exit(0)
+
+logging.basicConfig(level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S',
+                    format='%(asctime)-15s - [%(levelname)s] %(module)s: %(message)s', )
 
 parser = argparse.ArgumentParser(description='Receives a decimal code via a 433/315MHz GPIO device')
 parser.add_argument('-g', dest='gpio', type=int, default=27,
                     help="GPIO pin (Default: 27)")
 args = parser.parse_args()
 
-#signal.signal(signal.SIGINT, exithandler)
+signal.signal(signal.SIGINT, exithandler)
 rfdevice = RFDevice(args.gpio)
 rfdevice.enable_rx()
 timestamp = None
-#logging.info("Listening for codes on GPIO " + str(args.gpio))
-print('Listening')
+logging.info("Listening for codes on GPIO " + str(args.gpio))
+#print('Listening')
 while True:
     if rfdevice.rx_code_timestamp != timestamp:
         timestamp = rfdevice.rx_code_timestamp
-#        logging.info(str(rfdevice.rx_code) +
-#                     " [pulselength " + str(rfdevice.rx_pulselength) +
-#                     ", protocol " + str(rfdevice.rx_proto) + "]")
-        print(str(rfdevice.rx_code) +
+        code=str(rfdevice.rx_code)
+        print(code)
+        if code[:2]==code[-2:]=='69':
+            num=int(code[2:-2])
+            print('{}: {}'.format(num,light_args[num]))
+            sendMore(**light_args[num])
+        logging.info(str(rfdevice.rx_code) +
                      " [pulselength " + str(rfdevice.rx_pulselength) +
                      ", protocol " + str(rfdevice.rx_proto) + "]")
+#        print(str(rfdevice.rx_code) +
+#                     " [pulselength " + str(rfdevice.rx_pulselength) +
+#                     ", protocol " + str(rfdevice.rx_proto) + "]")
     time.sleep(0.05)
 rfdevice.cleanup()
